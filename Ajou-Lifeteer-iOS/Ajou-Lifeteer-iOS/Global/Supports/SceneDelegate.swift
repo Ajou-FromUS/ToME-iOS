7eea8b6c74cad18e47787a55e1f83201d1d481a2
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-07-27 00:50:08 +0900

    [Feat] #9 - 메인 랜딩페이지 UI 구현

--- Ajou-Lifeteer-iOS/Ajou-Lifeteer-iOS/Global/Supports/SceneDelegate.swift
+++ Ajou-Lifeteer-iOS/Ajou-Lifeteer-iOS/Global/Supports/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let nav = UINavigationController(rootViewController: SignInVC())
+        let nav = UINavigationController(rootViewController: LandingPageMainVC())
         window.rootViewController = nav
         self.window = window
         window.makeKeyAndVisible()
---
+++ Ajou-Lifeteer-iOS/Ajou-Lifeteer-iOS/Presentation/Landing/VC/FirstDetailLandingPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  FirstDetailLandingPageVC.swift
+//  Ajou-Lifeteer-iOS
+//
+//  Created by 몽이 누나 on 2023/07/27.
+//
+
+import UIKit
+
+class FirstDetailLandingPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
