560fc2799375ade32b21aed466e4f51dacb97f34
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-09-24 17:25:35 +0900

    [Feat] #19 - 파일명 변경

---
+++ Ajou-Lifeteer-iOS/Ajou-Lifeteer-iOS/Presentation/Talking/VC/TalkingMainVC.swift
@@ -0,0 +1,36 @@
+//
+//  TalkingMainVC.swift
+//  Ajou-Lifeteer-iOS
+//
+//  Created by 몽이 누나 on 2023/07/10.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class TalkingMainVC: UIViewController {
+
+    // MARK: - UI Components
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+// MARK: - UI & Layout
+
+extension TalkingMainVC {
+    private func setUI() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+}
